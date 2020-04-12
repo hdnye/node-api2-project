b91e208bd11054d2b1851b43b42afbf0615c793d
hdnye <heatherdnye@gmail.com>
2020-04-12 17:45:37 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express();
+
+server.use('/', (req, res) => {
+    res.status(200).send('Hello from express app running')
+});
+
+server.listen(8000, () => {
+    console.log('server listening on port 8000')
+});
